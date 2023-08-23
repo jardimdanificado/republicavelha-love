4f045640936898efe1e3e1278bde7894e5ffd744
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-23 11:08:47 -0300

    experimental

--- republicanova/init.lua
+++ republicanova/init.lua
@@ -1,8 +1,8 @@
 local modulo = {}
 local requirePath = love.filesystem.getRequirePath()
 love.filesystem.setRequirePath((mocegui and 'mocegui/luatils/?.lua' or '') .. 'republicanova/?.lua' .. requirePath)
-package.path = (mocegui and 'mocegui/?.lua' or '') .. 'republicanova/?.lua' .. ";" .. package.path
-modulo.util = require(mocegui and 'luatils' or 'luatils')
+package.path = 'republicanova/?.lua' .. ";" .. package.path
+modulo.util = require(mocegui and 'mocegui/luatils' or 'luatils')
 modulo.types = require('types')
 modulo.terrain = require('terrain')
 modulo.plants = modulo.types.plants
